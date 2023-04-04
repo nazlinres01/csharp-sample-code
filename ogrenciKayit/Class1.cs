c9c59f71b9b78a4451298b4d4e80894ec6680346
Nazlı Nur ESMERAY <96131988+Privatexx01@users.noreply.github.com>
2023-04-04 21:04:18 +0300

    Add files via upload

---
+++ ogrenciKayit/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    class Ogrenci
+    {
+        public int OgrenciNo;
+        public string OgrenciAdi;
+        public Ogrenci()
+        {
+            Console.WriteLine("KAYIT OLUŞTURULDU");
+        }
+
+        static Ogrenci()
+        {
+            Console.WriteLine("static kurucusu çalıştı");
+        }
+
+
+
+
+
+    }
+}
---
+++ ogrenciKayit/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ogrenci Ogr = new Ogrenci();
+            Ogr.OgrenciAdi = "Nazli";
+            Ogr.OgrenciNo = 191210074;
+            Console.WriteLine(Ogr.OgrenciAdi);
+            Console.WriteLine(Ogr.OgrenciNo);
+            Console.ReadKey();
+        }
+    }
+}
