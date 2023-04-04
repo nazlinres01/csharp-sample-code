3b6c1469bd928b91086aebc0647f8577b7555ac7
Nazlı Nur ESMERAY <96131988+Privatexx01@users.noreply.github.com>
2023-04-04 21:24:06 +0300

    Add files via upload

---
+++ windowsForm/zaman/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp17
+{
+    public partial class Form1 : Form
+    {
+        int i = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            textBox1.Text = textBox1.Text + i.ToString();
+            i++;
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            textBox1.Text = "";
+            i = 0;
+
+        }
+    }
+}
